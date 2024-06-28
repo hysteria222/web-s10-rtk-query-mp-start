4922eb1056155987002479d1f97055cca4ff56f5
Owner <129130014+hysteria222@users.noreply.github.com>
2024-06-28 12:26:10 -0500

    changed api slice and cannot get query to do anything but fail, pushing to do research.

--- frontend/state/quotesApi.js
+++ frontend/state/quotesApi.js
@@ -1,1 +1,28 @@
 // create your RTK Query endpoints here
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const quotesApi = createApi({
+    reducerPath: 'quotesApi', 
+    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:9009/api/quotes'}),
+    endpoints: builder => ({
+        getQuotes: builder.query({
+            query: () => 'quotes',
+        }),
+        createQuote: builder.mutation({
+
+        }),
+        toggleQuote: builder.mutation({
+
+        }),
+        deleteQuote: builder.mutation({
+
+        })
+    })
+})
+
+export const {
+    useGetQuotesQuery, 
+    useToggleMutation,
+    useCreateMutation, 
+    useDeleteMutation,  
+} = quotesApi
